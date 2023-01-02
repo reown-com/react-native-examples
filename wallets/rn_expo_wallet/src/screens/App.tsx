94bd630aa6115bfb0698e285859693c3049605f4
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2023-01-02 18:23:59 +0100

    init with sign client

--- wallets/rn_expo_wallet/src/screens/App.tsx
+++ wallets/rn_expo_wallet/src/screens/App.tsx
@@ -1,15 +1,51 @@
 import { registerRootComponent } from "expo";
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import "@walletconnect/react-native-compat";
+import SignClient from "@walletconnect/sign-client";
+import { ENV_PROJECT_ID, ENV_RELAY_URL } from "@env";
 
 export default function App() {
+  const [signClient, setSignClient] = useState<SignClient>();
+
+  async function createClient() {
+    try {
+      const client = await SignClient.init({
+        projectId: ENV_PROJECT_ID,
+        relayUrl: ENV_RELAY_URL,
+      });
+      setSignClient(client);
+      await subscribeToEvents(client);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function subscribeToEvents(client: SignClient) {
+    if (!client) {
+      throw Error("No events to subscribe to b/c the client does not exist");
+    }
+
+    try {
+      client.on("session_delete", () => {
+        console.log("user disconnected the session from their wallet");
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {}, [signClient]);
+
   return (
     <View style={styles.container}>
       <Text>WalletConnect</Text>
       <Text>Sign V2 Expo Examples</Text>
-      <Text>Connected: </Text>
-      <Button title="Connect" onPress={() => console.log("hi")} />
+      <Text>Connected: {signClient ? "true" : "false"} </Text>
+      <Button
+        title={!signClient ? "Connect" : "Connected"}
+        onPress={() => createClient()}
+      />
     </View>
   );
 }
