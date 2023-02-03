f02757c6fa21f08601c1c28ea190e9d654efc4e0
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2023-02-03 11:32:06 +0100

    chore: revert for other repo

--- wallets/rn_cli_wallet/src/utils/SignClient.ts
+++ wallets/rn_cli_wallet/src/utils/SignClient.ts
@@ -1,18 +1,14 @@
-import { Core } from '@walletconnect/core'
-import { ICore } from '@walletconnect/types'
-import { Web3Wallet, IWeb3Wallet } from '@walletconnect/web3wallet'
-
+import SignClient from '@walletconnect/sign-client';
 // @ts-expect-error - `@env` is a virtualised module via Babel config.
 import {ENV_PROJECT_ID, ENV_RELAY_URL} from '@env';
 
-export let web3wallet: IWeb3Wallet
-export let core: ICore
+export let signClient: SignClient;
 
-export async function createWeb3Wallet() {
+export async function createSignClient() {
   console.log('[CONFIG] ENV_PROJECT_ID:', ENV_PROJECT_ID);
   console.log('[CONFIG] ENV_RELAY_URL:', ENV_RELAY_URL);
 
-  web3wallet = await Web3Wallet.init({
+  signClient = await SignClient.init({
     logger: 'debug',
     projectId: ENV_PROJECT_ID,
     relayUrl: ENV_RELAY_URL,
@@ -23,5 +19,4 @@ export async function createWeb3Wallet() {
       icons: ['https://avatars.githubusercontent.com/u/37784886'],
     },
   });
-
 }
