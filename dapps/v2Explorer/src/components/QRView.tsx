5116eafbc8fdb404d7c4548e928db54489e36032
ignaciosantise <ignacio.santise@gmail.com>
2023-03-14 15:38:20 -0300

    Added copy to clipboard uri button

--- dapps/v2Explorer/src/components/QRView.tsx
+++ dapps/v2Explorer/src/components/QRView.tsx
@@ -1,8 +1,10 @@
 import React, {useEffect, useRef} from 'react';
-import {Animated, StyleSheet, useColorScheme} from 'react-native';
+import {Alert, Animated, StyleSheet, useColorScheme} from 'react-native';
 import {DEVICE_WIDTH} from '../constants/Platform';
 import NavigationHeader from './NavigationHeader';
 import QrCode from './QRCode';
+import CopyIcon from '../assets/Copy.png';
+import Clipboard from '@react-native-clipboard/clipboard';
 
 interface Props {
   uri: string;
@@ -13,6 +15,11 @@ function QRView({uri, onBackPress}: Props) {
   const fadeAnim = useRef(new Animated.Value(0)).current;
   const isDarkMode = useColorScheme() === 'dark';
 
+  const copyToClipboard = () => {
+    Clipboard.setString(uri);
+    Alert.alert('Copied to clipboard');
+  };
+
   useEffect(() => {
     Animated.timing(fadeAnim, {
       toValue: 1,
@@ -20,9 +27,15 @@ function QRView({uri, onBackPress}: Props) {
       useNativeDriver: true,
     }).start();
   }, [fadeAnim]);
+
   return (
     <Animated.View style={[styles.container, {opacity: fadeAnim}]}>
-      <NavigationHeader title="Scan the code" onBackPress={onBackPress} />
+      <NavigationHeader
+        title="Scan the code"
+        onBackPress={onBackPress}
+        actionIcon={CopyIcon}
+        onActionPress={copyToClipboard}
+      />
       <QrCode
         uri={uri}
         size={DEVICE_WIDTH * 0.9}
