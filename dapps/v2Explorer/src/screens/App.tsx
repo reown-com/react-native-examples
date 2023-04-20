dd69de676c6a07b6a06b6974edb182bab4bbe73f
ignaciosantise <ignacio.santise@gmail.com>
2023-04-20 10:25:17 -0300

    refactor: added w3m sdk and removed unused code

--- dapps/v2Explorer/src/screens/App.tsx
+++ dapps/v2Explorer/src/screens/App.tsx
@@ -1,28 +1,13 @@
 import 'react-native-get-random-values';
 import '@ethersproject/shims';
-
-import React, {useState, useCallback, useEffect} from 'react';
-import {
-  ActivityIndicator,
-  Alert,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  useColorScheme,
-  View,
-} from 'react-native';
-
+import React from 'react';
+import {SafeAreaView, StyleSheet, useColorScheme, View} from 'react-native';
 import '@walletconnect/react-native-compat';
-import useInitialization from '../hooks/useInitialization';
-import {
-  universalProviderSession,
-  universalProvider,
-  web3Provider,
-  clearSession,
-  createUniversalProviderSession,
-} from '../utils/UniversalProvider';
-import ExplorerModal from '../components/ExplorerModal';
+import {Web3Button, Web3Modal} from '@web3modal/react-native';
+
+// @ts-expect-error - `@env` is a virtualised module via Babel config.
+import {ENV_PROJECT_ID} from '@env';
+
 import {DarkTheme, LightTheme} from '../constants/Colors';
 
 function App(): JSX.Element {
@@ -30,137 +15,12 @@ function App(): JSX.Element {
   const backgroundColor = isDarkMode
     ? DarkTheme.background2
     : LightTheme.background2;
-  const [modalVisible, setModalVisible] = useState(false);
-  const [currentAccount, setCurrentAccount] = useState<string>();
-  const [currentWCURI, setCurrentWCURI] = useState<string>();
-
-  // Initialize universal provider
-  const initialized = useInitialization();
-
-  const close = () => {
-    setModalVisible(false);
-  };
-
-  const getAddress = useCallback(async () => {
-    try {
-      if (web3Provider) {
-        const signer = web3Provider.getSigner();
-        const currentAddress = await signer.getAddress();
-        setCurrentAccount(currentAddress);
-      }
-    } catch (err: unknown) {
-      Alert.alert('Error', 'Error getting the Address');
-    }
-  }, [setCurrentAccount]);
-
-  const onSessionCreated = useCallback(async () => {
-    getAddress();
-    setModalVisible(false);
-  }, [getAddress]);
-
-  const onSessionError = useCallback(async () => {
-    setModalVisible(false);
-    // TODO: Improve this, check why is alerting a lot, and check MaxListeners warning
-    // Alert.alert('Error', 'Error creating session');
-  }, []);
-
-  const onSessionDelete = useCallback(
-    async ({topic}: {topic: string}) => {
-      if (topic === universalProviderSession?.topic) {
-        clearSession();
-        setCurrentAccount(undefined);
-        setCurrentWCURI(undefined);
-      }
-    },
-    [setCurrentAccount],
-  );
-
-  const onConnect = useCallback(async () => {
-    createUniversalProviderSession({
-      onSuccess: onSessionCreated,
-      onFailure: onSessionError,
-    });
-    setModalVisible(true);
-  }, [onSessionCreated, onSessionError]);
-
-  const onDisconnect = useCallback(async () => {
-    try {
-      await universalProvider.disconnect();
-      clearSession();
-      setCurrentAccount(undefined);
-      setCurrentWCURI(undefined);
-    } catch (err: unknown) {
-      Alert.alert('Error', 'Error disconnecting');
-    }
-  }, []);
-
-  const subscribeToEvents = useCallback(async () => {
-    if (universalProvider) {
-      universalProvider.on('display_uri', (uri: string) => {
-        setCurrentWCURI(uri);
-      });
-
-      // Subscribe to session ping
-      universalProvider.on('session_ping', ({id, topic}) => {
-        console.log('session_ping', id, topic);
-      });
-
-      // Subscribe to session event
-      universalProvider.on('session_event', ({event, chainId}) => {
-        console.log('session_event', event, chainId);
-      });
-
-      // Subscribe to session update
-      universalProvider.on('session_update', ({topic, params}) => {
-        console.log('session_update', topic, params);
-      });
-
-      // Subscribe to session delete
-      universalProvider.on('session_delete', onSessionDelete);
-    }
-  }, [onSessionDelete]);
-
-  useEffect(() => {
-    if (initialized) {
-      subscribeToEvents();
-    }
-  }, [initialized, subscribeToEvents]);
 
   return (
     <SafeAreaView style={[styles.safeArea, {backgroundColor}]}>
       <View style={[styles.container, {backgroundColor}]}>
-        {currentAccount ? (
-          <View style={styles.container}>
-            <Text style={[styles.text, isDarkMode && styles.whiteText]}>
-              Address: {currentAccount}
-            </Text>
-            <TouchableOpacity
-              style={[
-                styles.blueButton,
-                styles.disconnectButton,
-                isDarkMode && styles.blueButtonDark,
-              ]}
-              onPress={onDisconnect}>
-              <Text style={styles.blueButtonText}>Disconnect</Text>
-            </TouchableOpacity>
-          </View>
-        ) : (
-          <TouchableOpacity
-            onPress={onConnect}
-            style={[styles.blueButton, isDarkMode && styles.blueButtonDark]}
-            disabled={!initialized}>
-            {initialized ? (
-              <Text style={styles.blueButtonText}>Connect Wallet</Text>
-            ) : (
-              <ActivityIndicator size="small" color="white" />
-            )}
-          </TouchableOpacity>
-        )}
-        <ExplorerModal
-          modalVisible={modalVisible}
-          close={close}
-          currentWCURI={currentWCURI}
-        />
+        <Web3Button />
+        <Web3Modal projectId={ENV_PROJECT_ID} />
       </View>
     </SafeAreaView>
   );
@@ -177,33 +37,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 8,
-  },
-  text: {
-    fontWeight: '700',
-  },
-  whiteText: {
-    color: 'white',
-  },
-  blueButton: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: LightTheme.accent,
-    borderRadius: 20,
-    width: 150,
-    height: 50,
-    borderWidth: 1,
-    borderColor: LightTheme.overlayThin,
-  },
-  blueButtonDark: {
-    backgroundColor: DarkTheme.accent,
-    borderColor: DarkTheme.overlayThin,
-  },
-  blueButtonText: {
-    color: 'white',
-    fontWeight: '700',
-  },
-  disconnectButton: {
-    marginTop: 20,
   },
 });
