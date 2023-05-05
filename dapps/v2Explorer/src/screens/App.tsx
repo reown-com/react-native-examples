a68266eb2b62aa3cdd4c9c418ff776c00615046f
ignaciosantise <ignacio.santise@gmail.com>
2023-05-05 16:22:51 -0300

    feat: Updated sdk version, added method examples

--- dapps/v2Explorer/src/screens/App.tsx
+++ dapps/v2Explorer/src/screens/App.tsx
@@ -1,17 +1,18 @@
-import 'react-native-get-random-values';
-import '@ethersproject/shims';
 import React from 'react';
 import {SafeAreaView, StyleSheet, useColorScheme, View} from 'react-native';
 import '@walletconnect/react-native-compat';
-import {Web3Button, Web3Modal} from '@web3modal/react-native';
+import {useWeb3Modal, Web3Button, Web3Modal} from '@web3modal/react-native';
 
 // @ts-expect-error - `@env` is a virtualised module via Babel config.
 import {ENV_PROJECT_ID} from '@env';
 
 import {DarkTheme, LightTheme} from '../constants/Colors';
+import {providerMetadata, sessionParams} from '../constants/Config';
+import {BlockchainActions} from '../components/BlockchainActions';
 
-function App(): JSX.Element {
+function App() {
   const isDarkMode = useColorScheme() === 'dark';
+  const {isConnected} = useWeb3Modal();
   const backgroundColor = isDarkMode
     ? DarkTheme.background2
     : LightTheme.background2;
@@ -19,8 +20,13 @@ function App(): JSX.Element {
   return (
     <SafeAreaView style={[styles.safeArea, {backgroundColor}]}>
       <View style={[styles.container, {backgroundColor}]}>
-        <Web3Button />
-        <Web3Modal projectId={ENV_PROJECT_ID} />
+        <Web3Button style={styles.web3Button} />
+        {isConnected && <BlockchainActions />}
+        <Web3Modal
+          projectId={ENV_PROJECT_ID}
+          providerMetadata={providerMetadata}
+          sessionParams={sessionParams}
+        />
       </View>
     </SafeAreaView>
   );
@@ -37,5 +43,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 8,
+  },
+  web3Button: {
+    width: 180,
   },
 });
