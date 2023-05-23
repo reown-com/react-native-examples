64955b5f09ba937d7ce3a8e6cf88ebcf562f2cfd
ignaciosantise <ignacio.santise@gmail.com>
2023-05-23 15:56:18 -0300

    made react-native-dapp work

--- dapps/ReactNativeMigrateDapp/src/App.tsx
+++ dapps/ReactNativeMigrateDapp/src/App.tsx
@@ -5,11 +5,17 @@
  * @format
  */
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  useWalletConnect,
+  withWalletConnect,
+} from '@walletconnect/react-native-dapp';
 import React from 'react';
 import {
   SafeAreaView,
   StatusBar,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
@@ -18,6 +24,7 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const connector = useWalletConnect();
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -35,9 +42,20 @@ function App(): JSX.Element {
           backgroundColor: isDarkMode ? Colors.black : Colors.white,
         }}>
         <Text>Test</Text>
+        <TouchableOpacity onPress={() => connector.connect()}>
+          <Text>CONNECT</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 }
 
-export default App;
+export default withWalletConnect(App, {
+  clientMeta: {
+    description: 'Connect with WalletConnect',
+  },
+  redirectUrl: 'yourappscheme://',
+  storageOptions: {
+    asyncStorage: AsyncStorage,
+  },
+});
