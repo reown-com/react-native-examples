d77e9b1040cea3bbae80fad2905b15cc6b77892b
ignaciosantise <ignacio.santise@gmail.com>
2023-05-24 13:46:20 -0300

    feat: show connection info

--- dapps/ReactNativeMigrateDapp/src/screens/DappConnect.tsx
+++ dapps/ReactNativeMigrateDapp/src/screens/DappConnect.tsx
@@ -1,18 +1,47 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useWalletConnect } from '@walletconnect/react-native-dapp';
 
 function DappConnect() {
   const connector = useWalletConnect();
+  console.log(connector);
   return (
     <View style={styles.container}>
+      {connector?.connected && (
+        <View>
+          <Image
+            style={styles.icon}
+            source={{ uri: connector.peerMeta?.icons[0] }}
+          />
+          <View style={styles.card}>
+            <Text style={styles.title}>Connected to:</Text>
+            <Text>{connector.peerMeta?.name}</Text>
+          </View>
+          <View style={styles.card}>
+            <Text style={styles.title}>Accounts:</Text>
+            {connector.accounts?.map(account => (
+              <Text key={account}>{account}</Text>
+            ))}
+          </View>
+          <View style={styles.card}>
+            <Text style={styles.title}>Chain ID:</Text>
+            <Text>{connector.chainId}</Text>
+          </View>
+        </View>
+      )}
       {connector?.connected ? (
-        <TouchableOpacity onPress={() => connector.killSession()}>
-          <Text>Kill session</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => connector.killSession()}>
+          <Text style={styles.buttonText}>Kill session</Text>
         </TouchableOpacity>
       ) : (
-        <TouchableOpacity onPress={() => connector.connect()}>
-          <Text>Connect</Text>
-        </TouchableOpacity>
+        <View style={styles.connectContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => connector.connect()}>
+            <Text style={styles.buttonText}>Connect</Text>
+          </TouchableOpacity>
+        </View>
       )}
     </View>
   );
@@ -23,8 +52,38 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     height: '100%',
     width: '100%',
-    justifyContent: 'center',
     alignItems: 'center',
+  },
+  connectContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    marginVertical: 16,
+    paddingHorizontal: 4,
+  },
+  button: {
+    backgroundColor: '#3396FF',
+    borderRadius: 25,
+    width: 200,
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 32,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    borderRadius: 20,
   },
 });
 
--- dapps/ReactNativeMigrateDapp/src/screens/Home.tsx
+++ dapps/ReactNativeMigrateDapp/src/screens/Home.tsx
@@ -11,7 +11,9 @@ function Home({ navigation }: Props) {
         This example is meant to help you migrate from react-native-dapp to
         @web3modal/react-native
       </Text>
-      <Text>react-native-dapp will be deprecated the 28th June</Text>
+      <Text style={styles.importantText}>
+        WalletConnect v1 will be deprecated the 28th June
+      </Text>
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('DappConnect')}>
@@ -26,6 +28,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 16,
   },
   button: {
     backgroundColor: '#3396FF',
@@ -39,6 +42,11 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     fontWeight: 'bold',
+  },
+  importantText: {
+    marginVertical: 16,
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
 
