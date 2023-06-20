149a5d79b59ec017023dd7b586ba3544956e0ca1
Cali93 <32299095+Cali93@users.noreply.github.com>
2023-06-20 14:12:11 +0300

    chore(page-visibility): W3I page visibility props

--- wallets/rn_cli_w3i_wallet/src/components/Modals/W3InboxModal.tsx
+++ wallets/rn_cli_w3i_wallet/src/components/Modals/W3InboxModal.tsx
@@ -1,7 +1,6 @@
-import React, {useCallback, useRef} from 'react';
-import {TouchableOpacity, Text} from 'react-native';
+import React, {ReactNode, useCallback, useMemo, useRef} from 'react';
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
 import Modal from 'react-native-modal';
-// import {SafeAreaView} from 'react-native-safe-area-context';
 import {WebView, WebViewMessageEvent} from 'react-native-webview';
 import {currentETHAddress} from '../../utils/clients';
 import {generateResponse} from '../../utils/Web3Inbox';
@@ -9,21 +8,38 @@ import {generateResponse} from '../../utils/Web3Inbox';
 interface W3InboxModalProps {
   visible: boolean;
   setVisible: (arg0: boolean) => void;
+  chatEnabled?: boolean;
+  pushEnabled?: boolean;
+  settingsEnabled?: boolean;
+  closeButton?: ReactNode;
 }
 
-export function W3InboxModal({visible, setVisible}: W3InboxModalProps) {
+export function W3InboxModal({
+  visible,
+  setVisible,
+  chatEnabled = true,
+  pushEnabled = true,
+  settingsEnabled = true,
+}: W3InboxModalProps) {
   const webViewRef = useRef<WebView>(null);
+
   const RN = 'reactnative';
   const WEB3INBOX_BASE_URL = 'https://web3inbox-dev-hidden.vercel.app/';
-  const WEB3INBOX_QUERY_PARAMS = `?chatProvider=${RN}&authProvider=${RN}&pushProvider=${RN}&account=${currentETHAddress}`;
-  const WEB3INBOX_URL = `${WEB3INBOX_BASE_URL}${WEB3INBOX_QUERY_PARAMS}`;
+  const WEB3INBOX_PROVIDER_QUERY_PARAMS = `?chatProvider=${RN}&authProvider=${RN}&pushProvider=${RN}&account=${currentETHAddress}`;
+
+  const WEB3INBOX_VISIBILITY_QUERY_PARAMS = useMemo(
+    () =>
+      `&chatEnabled=${chatEnabled}&pushEnabled=${pushEnabled}&settingsEnabled=${settingsEnabled}`,
+    [chatEnabled, pushEnabled, settingsEnabled],
+  );
+
+  const WEB3INBOX_URL = `${WEB3INBOX_BASE_URL}${WEB3INBOX_PROVIDER_QUERY_PARAMS}${WEB3INBOX_VISIBILITY_QUERY_PARAMS}`;
 
   const handleMessage = useCallback(async (event: WebViewMessageEvent) => {
     if (!event.nativeEvent.data) {
       return;
     }
     const message = JSON.parse(event.nativeEvent.data);
-    console.log({message});
     const injectedJavascript = await generateResponse(
       message.targetClient,
       message,
@@ -33,17 +49,16 @@ export function W3InboxModal({visible, setVisible}: W3InboxModalProps) {
     }
     webViewRef.current?.injectJavaScript(injectedJavascript);
   }, []);
+
   return (
     <Modal backdropOpacity={0.6} isVisible={visible}>
       <TouchableOpacity onPress={() => setVisible(false)}>
-        <Text style={{color: 'red', marginTop: 20, fontWeight: 'bold'}}>
-          CLOSE
-        </Text>
+        <Text style={styles.closeText}>CLOSE</Text>
       </TouchableOpacity>
       <WebView
         ref={webViewRef}
         source={{uri: WEB3INBOX_URL}}
-        style={{flex: 1, borderRadius: 20, marginTop: 32}}
+        style={styles.webview}
         originWhitelist={['*']}
         onMessage={handleMessage}
       />
@@ -51,16 +66,28 @@ export function W3InboxModal({visible, setVisible}: W3InboxModalProps) {
   );
 }
 
-// const styles = StyleSheet.create({
-//   modalContainer: {
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     borderRadius: 34,
-//     paddingTop: 30,
-//     backgroundColor: 'rgba(242, 242, 247, 0.9)',
-//     width: '100%',
-//     position: 'absolute',
-//     bottom: 44,
-//   },
-// });
+const styles = StyleSheet.create({
+  webview: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    paddingTop: 30,
+    backgroundColor: 'white',
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    bottom: 5,
+  },
+  closeText: {
+    color: 'red',
+    marginTop: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+    fontWeight: 'bold',
+    alignSelf: 'flex-end',
+    backgroundColor: 'white',
+    padding: 4,
+  },
+});
