7d55a37b0cea196ab0b9a079057f89b4c2fa279b
ignaciosantise <ignacio.santise@gmail.com>
2023-07-13 15:26:38 -0300

    Implemented walletconnectmodal basic usage

--- dapps/rn_dapp_0_68_5/App.js
+++ dapps/rn_dapp_0_68_5/App.js
@@ -1,9 +1,42 @@
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import { WalletConnectModal, useWalletConnectModal } from '@walletconnect/modal-react-native';
 
 const App = () => {
+  const { open, isConnected, address, provider } = useWalletConnectModal();
+  const PROJECT_ID = "ADD_YOUR_PROJECT_ID_HERE"
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Test</Text>
+      {isConnected ?
+        <View>
+          <Text>Connected to {address}</Text>
+          <TouchableOpacity onPress={() => provider?.disconnect()}>
+            <Text>Disconnect</Text>
+        </TouchableOpacity>
+        </View>
+         :
+        <TouchableOpacity onPress={open}>
+          <Text>Connect</Text>
+        </TouchableOpacity>
+      }
+      <WalletConnectModal
+        projectId={PROJECT_ID}
+        providerMetadata={{
+          name: 'WalletConnect',
+          description: 'Scan qrcode to connect',
+          url: 'https://walletconnect.org',
+          icons: ['https://walletconnect.org/walletconnect-logo.png'],
+          redirect: {
+            native:'walletconnect://',
+          }
+        }}
+      />
     </SafeAreaView>
   );
 };
@@ -11,8 +44,8 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent:'center',
+    alignItems:'center'
   }
 });
 
