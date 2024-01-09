613aec632b0c2295b1ec904c31332eb4689fe6d4
ignaciosantise <ignacio.santise@gmail.com>
2024-01-09 10:18:39 -0300

    chore: bump modal dep to 1.1.0

--- dapps/ModalViem/src/App.tsx
+++ dapps/ModalViem/src/App.tsx
@@ -7,6 +7,7 @@ import {
   View,
 } from 'react-native';
 
+import '@walletconnect/react-native-compat';
 import {
   useWalletConnectModal,
   WalletConnectModal,
@@ -113,6 +114,7 @@ function App(): JSX.Element {
       account: address,
       to: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045', // vitalik.eth
       value: parseEther('0.001'),
+      data: '0x',
     });
 
     return {
