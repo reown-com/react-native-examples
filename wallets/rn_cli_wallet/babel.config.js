5a730ada223cbb3eb4e32ff2fe093bb84445e16d
ignaciosantise <nacho@walletconnect.com>
2024-01-25 15:03:55 -0300

    feat: replace crypto lib (#120)
    * fix: using quick-crypto to generate wallet from mnemonic

--- wallets/rn_cli_wallet/babel.config.js
+++ wallets/rn_cli_wallet/babel.config.js
@@ -2,6 +2,16 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
+      'module-resolver',
+      {
+        alias: {
+          'crypto': 'react-native-quick-crypto',
+          'stream': 'stream-browserify',
+          'buffer': '@craftzdog/react-native-buffer',
+        },
+      },
+    ],
+    [
       'module:react-native-dotenv',
       {
         envName: 'APP_ENV',
