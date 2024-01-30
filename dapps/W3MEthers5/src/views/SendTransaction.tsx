f1e6c56a6205e59bc7f921994547a00cfae4b955
ignaciosantise <nacho@walletconnect.com>
2024-01-30 17:39:09 -0300

    chore: added w3m + ethers5 example

---
+++ dapps/W3MEthers5/src/views/SendTransaction.tsx
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import {Button} from '@web3modal/ui-react-native';
+
+import {RequestModal} from '../components/RequestModal';
+
+import {
+  useWeb3ModalAccount,
+  useWeb3ModalProvider,
+} from '@web3modal/ethers5-react-native';
+import {ethers} from 'ethers';
+
+export function SendTransaction() {
+  const [requestModalVisible, setRequetsModalVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<string | undefined>();
+  const [error, setError] = useState(false);
+  const {walletProvider} = useWeb3ModalProvider();
+  const {isConnected} = useWeb3ModalAccount();
+
+  const onPress = async () => {
+    if (!isConnected || !walletProvider) {
+      return;
+    }
+
+    setData(undefined);
+    setError(false);
+    setIsLoading(true);
+    setRequetsModalVisible(true);
+
+    try {
+      const ethersProvider = new ethers.providers.Web3Provider(walletProvider);
+      const signer = ethersProvider.getSigner();
+      const address = await signer.getAddress();
+      const tx = {
+        to: address,
+        value: ethers.utils.parseEther('0.0001'),
+        data: '0x',
+      };
+      const txResponse = await signer.sendTransaction(tx);
+      setData(txResponse.hash);
+    } catch {
+      setError(true);
+    }
+    setIsLoading(false);
+  };
+
+  return isConnected ? (
+    <View>
+      <Button disabled={requestModalVisible} onPress={onPress}>
+        Send transaction
+      </Button>
+
+      <RequestModal
+        isVisible={requestModalVisible}
+        isLoading={isLoading}
+        rpcResponse={data ? data : undefined}
+        rpcError={error ? 'Error sending transaction' : undefined}
+        onClose={() => setRequetsModalVisible(false)}
+      />
+    </View>
+  ) : null;
+}
