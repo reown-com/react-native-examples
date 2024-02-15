fbbaa3e53d209eaccadf255d6aded717a0f5f057
enesozturk <enesozturk.d@gmail.com>
2024-02-16 01:12:34 +0300

    refactor: update babel config

--- wallets/rn_cli_wallet/babel.config.js
+++ wallets/rn_cli_wallet/babel.config.js
@@ -1,6 +1,19 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
   plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
     [
       'module-resolver',
       {
@@ -21,19 +34,6 @@ module.exports = {
           stream: 'stream-browserify',
           buffer: '@craftzdog/react-native-buffer',
         },
-      },
-    ],
-    [
-      'module:react-native-dotenv',
-      {
-        envName: 'APP_ENV',
-        moduleName: '@env',
-        path: '.env',
-        blocklist: null,
-        allowlist: null,
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
       },
     ],
   ],
