55e364012b202c34887415c3603cd61ef23b4b94
ignaciosantise <nacho@walletconnect.com>
2024-06-14 17:58:02 -0300

    chore: removed notifications code causing issues, updated quick-crypto

--- wallets/rn_cli_wallet/src/screens/Subscriptions/styles.ts
+++
@@ -1,9 +0,0 @@
-import {StyleSheet} from 'react-native';
-
-export default StyleSheet.create({
-  loaderContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
