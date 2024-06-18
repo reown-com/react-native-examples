eea0739d40779a3aebb3223181ff2469b43e4782
Ignacio Santise <nacho@walletconnect.com>
2024-06-18 17:10:58 -0300

    chore: added universal link config to dapp and wallet (#156)

--- wallets/rn_cli_wallet/ios/RNWeb3Wallet/AppDelegate.mm
+++ wallets/rn_cli_wallet/ios/RNWeb3Wallet/AppDelegate.mm
@@ -17,12 +17,21 @@
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-// Deeplink
+// Enable Deeplinks
 - (BOOL)application:(UIApplication *)application
    openURL:(NSURL *)url
    options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
 {
   return [RCTLinkingManager application:application openURL:url options:options];
+}
+
+// Enable Universal Links
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
