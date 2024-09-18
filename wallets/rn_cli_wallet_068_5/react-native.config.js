d464c08a4ae6a438c7a3efc29f63980ce052b23b
ignaciosantise <25931366+ignaciosantise@users.noreply.github.com>
2024-09-18 16:41:07 -0300

    chore: removed react-native 0.68.5 examples

--- wallets/rn_cli_wallet_068_5/react-native.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  project: {
-    ios: {},
-    android: {}, // grouped into "project"
-  },
-  assets: ['./src/assets/fonts/'], // stays the same
-};
--- wallets/rn_cli_wallet_068_5/src/utils/Text.ts
+++
@@ -1,7 +0,0 @@
-export const TextContent = {
-  welcomeTitle: 'Welcome',
-  welcomeDescription:
-    'We made this Example Wallet App to help developers integrate the WalletKit SDK and provide an amazing experience to their users.',
-  GetStartedButton: 'Get Started',
-  AppsTitle: 'Apps',
-};
