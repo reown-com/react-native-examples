bfa890a14f507bd9ecec8999e30b28be42539e52
Gancho Radkov <ganchoradkov93@gmail.com>
2024-12-12 12:51:54 +0200

    feat: chain abstraction handling

---
+++ wallets/rn_cli_wallet/src/modals/RequestModalV2/RequestModalV2.tsx
@@ -0,0 +1,219 @@
+import {ReactNode, useMemo} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {LinearGradient} from 'react-native-linear-gradient';
+import {CoreTypes} from '@walletconnect/types';
+
+import {ModalHeader} from './ModalHeader';
+import {useTheme} from '@/hooks/useTheme';
+import {useSnapshot} from 'valtio';
+import SettingsStore from '@/store/SettingsStore';
+import VerifyInfoBox from '@/components/VerifyInfoBox';
+import {ActionButton} from '@/components/ActionButton';
+
+export interface RequestModalProps {
+  children: ReactNode;
+  metadata?: CoreTypes.Metadata;
+  heading: string;
+  onApprove: () => void;
+  onReject: () => void;
+  approveLoader?: boolean;
+  rejectLoader?: boolean;
+  intention?: string;
+  isLinkMode?: boolean;
+  disableApprove?: boolean;
+}
+
+export function RequestModalV2({
+  children,
+  heading,
+  onApprove,
+  onReject,
+  approveLoader,
+  rejectLoader,
+  disableApprove,
+}: RequestModalProps) {
+  const {currentRequestVerifyContext} = useSnapshot(SettingsStore.state);
+  return (
+    <View style={styles.container}>
+      <ModalHeader heading={heading} />
+      {children}
+      <ModalFooter
+        onApprove={onApprove}
+        onReject={onReject}
+        approveLoader={approveLoader}
+        rejectLoader={rejectLoader}
+        verifyContext={currentRequestVerifyContext}
+        disableApprove={disableApprove}
+      />
+    </View>
+  );
+}
+
+function ModalFooter({
+  onApprove,
+  onReject,
+  approveLoader,
+  rejectLoader,
+  verifyContext,
+  disableApprove,
+}: any) {
+  const Theme = useTheme();
+  const validation = verifyContext?.verified.validation;
+  const isScam = verifyContext?.verified.isScam;
+
+  const approveButtonColors = useMemo(() => {
+    let gradient;
+    let textColor;
+    if (disableApprove) {
+      gradient = ['transparent'];
+      textColor = '#9A9A9A';
+    } else {
+      gradient = ['#732BCC', '#076CF1'];
+      textColor = 'white';
+    }
+    return {
+      gradient,
+      textColor,
+    };
+  }, [disableApprove]);
+
+  return (
+    <View style={styles.container}>
+      <VerifyInfoBox
+        isScam={isScam}
+        validation={validation}
+        style={{marginBottom: 15}}
+      />
+      <View style={stylesFooter.buttonContainer}>
+        <ActionButton
+          loading={rejectLoader}
+          disabled={approveLoader || rejectLoader}
+          style={[stylesFooter.button, stylesFooter.declineButton]}
+          textStyle={stylesFooter.rejectText}
+          onPress={onReject}
+          secondary>
+          Cancel
+        </ActionButton>
+        <LinearGradient
+          colors={approveButtonColors.gradient} // Gradient colors
+          start={{x: 0, y: 0}} // Gradient start point
+          end={{x: 1, y: 1}} // Gradient end point
+          style={stylesFooter.button}>
+          <ActionButton
+            loading={approveLoader}
+            disabled={approveLoader || disableApprove}
+            style={[stylesFooter.button, stylesFooter.appoveButton]}
+            textStyle={[
+              stylesFooter.approveText,
+              {color: approveButtonColors.textColor},
+            ]}
+            onPress={onApprove}>
+            Approve
+          </ActionButton>
+        </LinearGradient>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column', // Main container stacks rows vertically
+    padding: 10,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    width: 'auto',
+    backgroundColor: '#2A2A2A',
+  },
+  section: {
+    width: '100%',
+    backgroundColor: '#252525',
+    borderRadius: 20,
+    display: 'flex',
+    padding: 5,
+    marginBottom: 10,
+  },
+  innerSection: {
+    backgroundColor: '#2A2A2A',
+    borderRadius: 20,
+  },
+  alignInCenter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row', // Each row splits into two columns
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 44,
+    paddingLeft: 20,
+    paddingEnd: 20,
+  },
+  chainLogo: {
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+  },
+  column: {
+    alignItems: 'center', // Center content horizontally in the column
+  },
+  iconColumn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textMuted: {
+    color: '#9A9A9A',
+    fontSize: 14,
+    fontWeight: 400,
+    lineHeight: 16,
+    textAlign: 'left',
+  },
+  textMid: {
+    fontSize: 14,
+    lineHeight: 16,
+  },
+  textSm: {
+    fontSize: 12,
+    lineHeight: 14,
+  },
+  textMain: {
+    color: '#ffffff',
+    fontWeight: 500,
+    textAlign: 'right',
+  },
+});
+
+const stylesFooter = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  declineButton: {
+    backgroundColor: 'transparent',
+  },
+  appoveButton: {
+    backgroundColor: 'transparent',
+    width: '100%',
+  },
+  button: {
+    width: '48%',
+    height: 48,
+    borderRadius: 40,
+    borderWidth: 1,
+    borderColor: '#363636',
+  },
+  rejectText: {
+    fontWeight: '500',
+    fontSize: 16,
+    color: '#ffff',
+  },
+  approveText: {
+    fontWeight: '600',
+    fontSize: 16,
+  },
+});
--- wallets/rn_cli_wallet/src/utils/EIP155WalletUtil.ts
+++ wallets/rn_cli_wallet/src/utils/EIP155WalletUtil.ts
@@ -1,5 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import EIP155Lib from '../lib/EIP155Lib';
+import {ethers} from 'ethers';
+import {PresetsUtil} from '../utils/PresetsUtil';
+import {getAbiByPrefix, parseChainId} from './HelperUtil';
 
 export let wallet1: EIP155Lib;
 export let wallet2: EIP155Lib;
@@ -36,9 +39,203 @@ export async function createOrRestoreEIP155Wallet() {
     [address2]: wallet2,
   };
   eip155Addresses = Object.keys(eip155Wallets);
+  console.log('eip155Wallets:', eip155Wallets);
 
   return {
     eip155Wallets,
     eip155Addresses,
   };
+}
+
+export async function calculateEip155Gas(transaction: any, chainId: string) {
+  console.log('calculateEip155Gas:', chainId);
+  const chainData = PresetsUtil.getChainData(parseChainId(chainId));
+  console.log('chainData:');
+  // Define the sender (from) and receiver (to) addresses
+  const from = transaction.from;
+  const to = transaction.to; // Could be a contract address
+  const data = transaction.data; // Some contract interaction data
+
+  // Prepare the transaction object
+  const tx = {
+    from: from,
+    to: to,
+    data: data,
+  };
+
+  try {
+    let provider = new ethers.providers.JsonRpcProvider(chainData.rpcUrl);
+
+    // Fetch the latest block to get the base fee
+    const block = await provider.getBlock('latest');
+    if (!block) {
+      return;
+    }
+    const baseFee = block.baseFeePerGas;
+    const fees = await fetchGasPrice(parseChainId(chainId));
+
+    // You can adjust the priority fee based on current network conditions
+    const maxPriorityFeePerGas = fees.normal.priority_price.toString();
+    // console.log('fees:', fees);
+    // Calculate the max fee per gas (base fee + priority fee)
+    const maxFeePerGas = baseFee!.add(maxPriorityFeePerGas);
+
+    // use this node to estimate gas as it doesn't reject when the amount is greater than the balance
+    // very useful for chain abstraction
+    provider = new ethers.providers.JsonRpcProvider(
+      'https://endpoints.omniatech.io/v1/arbitrum/one/public',
+    );
+
+    // Estimate the gas limit for this transaction based on its size and complexity
+    const gasLimit = await provider.estimateGas(tx);
+
+    // Log the details of the gas fees
+    console.log(
+      'Base Fee:',
+      ethers.utils.formatUnits(baseFee!, 'gwei'),
+      'Gwei',
+    );
+    console.log(
+      'Max Priority Fee:',
+      ethers.utils.formatUnits(maxPriorityFeePerGas, 'gwei'),
+      'Gwei',
+    );
+    console.log(
+      'Max Fee per Gas:',
+      ethers.utils.formatUnits(maxFeePerGas, 'gwei'),
+      'Gwei',
+    );
+    console.log('Estimated Gas Limit:', gasLimit.toString());
+
+    // The total gas cost (just as an example, no sign and send in this code)
+    const estimatedGasCost = gasLimit.mul(maxFeePerGas);
+    console.log('Estimated Gas Cost (Wei):', estimatedGasCost.toString());
+
+    return {
+      maxFeePerGas: ethers.utils.formatUnits(maxFeePerGas, 'wei'),
+      maxPriorityFeePerGas: ethers.utils.formatUnits(
+        maxPriorityFeePerGas,
+        'wei',
+      ),
+      gasLimit,
+      totalGas: ethers.utils.formatUnits(estimatedGasCost, 'ether'),
+    };
+  } catch (error) {
+    console.error('Error fetching gas fees:', error);
+    return {
+      gasLimit: {hex: '0x02db52', type: 'BigNumber'},
+      maxFeePerGas: '1100735',
+      maxPriorityFeePerGas: '1100000',
+      totalGas: '0.00000020607740523',
+    };
+  }
+}
+
+const fetchGasPrice = async (chainId: string) => {
+  // const result = await fetch(
+  //   `https://tokentool.bitbond.com/api/gas-price-20240116?chain_id=${parseChainId(
+  //     chainId,
+  //   )}&timestamp=${Date.now()}`,
+  // );
+  // const data = await result.json();
+  // console.log('fetchGasPrice:', data);
+  // return data?.data;
+
+  const result = {
+    data: {
+      fast: {
+        estimated_seconds: 0,
+        front_tx_count: 0,
+        level: 'fast',
+        price: 11006500,
+        priority_price: 1200000,
+      },
+      normal: {
+        estimated_seconds: 0,
+        front_tx_count: 0,
+        level: 'normal',
+        price: 1200800,
+        priority_price: 1100000,
+      },
+      slow: {
+        estimated_seconds: 0,
+        front_tx_count: 0,
+        level: 'slow',
+        price: 1000600,
+        priority_price: 1000000,
+      },
+    },
+    success: true,
+  };
+  return result.data;
+};
+
+export async function calculateGasLimit(tx: any, chainId: string) {
+  const chainData = PresetsUtil.getChainData(parseChainId(chainId));
+  const provider = new ethers.providers.JsonRpcProvider(chainData.rpcUrl);
+
+  const result = await provider.estimateGas(tx);
+  return result;
+}
+
+export async function getNonce(address: string, chainId: string) {
+  const chainData = PresetsUtil.getChainData(parseChainId(chainId));
+  const provider = new ethers.providers.JsonRpcProvider(chainData.rpcUrl);
+
+  const nonce = await provider.getTransactionCount(address);
+  console.log('getNonce:', nonce);
+  return nonce;
+}
+
+export async function getTransferDetails(
+  data: string,
+  contractAddress: string,
+  chainId: string,
+) {
+  const prefix = data.slice(0, 10);
+  // Define the function ABI
+  const abi = getAbiByPrefix(prefix);
+  console.log('ABI:', abi);
+  // Create an Interface instance
+  const iabi = new ethers.utils.Interface(abi);
+  console.log('Interface data', data);
+  // Decode the transaction data
+  const decodedData = iabi.parseTransaction({
+    data: data,
+  });
+
+  const chainData = PresetsUtil.getChainData(parseChainId(chainId));
+  // Connect to the Ethereum network
+  const provider = new ethers.providers.JsonRpcProvider(chainData.rpcUrl);
+
+  const ERC20_ABI = [
+    'function decimals() view returns (uint8)',
+    'function symbol() view returns (string)',
+  ];
+
+  console.log('Token Address:', contractAddress);
+  // Create a contract instance
+  const tokenContract = new ethers.Contract(
+    contractAddress,
+    ERC20_ABI,
+    provider,
+  );
+
+  // Fetch decimals and symbol
+  try {
+    const decimals = await tokenContract.decimals();
+    const symbol = await tokenContract.symbol();
+
+    const amount =
+      parseInt(decodedData.args[1].toString(), 10) / 10 ** decimals;
+
+    console.log(`Token Symbol: ${symbol}`);
+    console.log(`Token Decimals: ${decimals}`);
+    console.log(`Total Amount: ${amount}`);
+
+    return {symbol, decimals, amount};
+  } catch (error) {
+    console.error('Error fetching token details:', error);
+    return null;
+  }
 }
