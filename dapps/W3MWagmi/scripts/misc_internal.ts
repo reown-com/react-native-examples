e98728d00b96cc3775a754c6513b83e62c5905bf
nacho <25931366+ignaciosantise@users.noreply.github.com>
2025-06-09 16:39:25 -0300

    chore: added maestro flows for native to native and native to web

--- dapps/W3MWagmi/scripts/misc_internal.ts
+++ dapps/W3MWagmi/scripts/misc_internal.ts
@@ -1,5 +1,3 @@
-import {Platform} from 'react-native';
-
 export const getMetadata = () => {
   return {
     name: 'AppKit + wagmi (internal)',
@@ -18,41 +16,12 @@ export const getCustomWallets = () => {
   const wallets = [
     {
       id: 'rn-wallet-internal',
-      name: 'Wallet(RN internal)',
-      image_url:
-        'https://github.com/reown-com/reown-docs/blob/main/static/assets/home/walletkitLogo.png?raw=true',
+      name: 'React Native Wallet',
+      image_url: 'https://avatars.githubusercontent.com/u/179229932',
       mobile_link: 'rn-web3wallet-internal://',
       link_mode: 'https://appkit-lab.reown.com/rn_walletkit_internal',
     },
-    {
-      id: 'flutter-wallet-internal',
-      name: 'Wallet(Flutter internal)',
-      image_url:
-        'https://github.com/reown-com/reown-docs/blob/main/static/assets/home/walletkitLogo.png?raw=true',
-      mobile_link: 'wcflutterwallet-internal://',
-      link_mode: 'https://appkit-lab.reown.com/flutter_walletkit_internal',
-    },
   ];
-
-  if (Platform.OS === 'android') {
-    wallets.push({
-      id: 'android-wallet-internal',
-      name: 'Wallet(Android internal)',
-      image_url:
-        'https://github.com/reown-com/reown-docs/blob/main/static/assets/home/walletkitLogo.png?raw=true',
-      mobile_link: 'kotlin-web3wallet://',
-      link_mode: 'https://appkit-lab.reown.com/wallet_internal',
-    });
-  } else if (Platform.OS === 'ios') {
-    wallets.push({
-      id: 'ios-wallet',
-      name: 'Wallet(iOS)',
-      image_url:
-        'https://github.com/reown-com/reown-docs/blob/main/static/assets/home/walletkitLogo.png?raw=true',
-      mobile_link: 'walletapp://',
-      link_mode: 'https://appkit-lab.reown.com/wallet',
-    });
-  }
 
   return wallets;
 };
