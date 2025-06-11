c36d46a12015873a8843207be7712ff7405316ea
nacho <25931366+ignaciosantise@users.noreply.github.com>
2025-06-11 15:09:38 -0300

    chore: changed android device

--- wallets/rn_cli_wallet/src/screens/Scan/index.tsx
+++ wallets/rn_cli_wallet/src/screens/Scan/index.tsx
@@ -64,7 +64,7 @@ export default function Scan({navigation}: Props) {
         />
       </TouchableOpacity>
 
-      {showCamera && device ? (
+      {showCamera && device != null ? (
         <Camera
           style={StyleSheet.absoluteFill}
           device={device}
