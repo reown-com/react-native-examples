8b8b2badfee0acc83e7c428afee51b94ceff6d54
Ignacio Santise <25931366+ignaciosantise@users.noreply.github.com>
2025-06-30 16:59:20 -0300

    chore: added appkit expo wagmi sample (#216)

---
+++ dapps/appkit-expo-wagmi/app/(tabs)/index.tsx
@@ -0,0 +1,50 @@
+import { AppKitButton } from '@reown/appkit-wagmi-react-native';
+import { Image } from 'expo-image';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <>
+      <ParallaxScrollView
+        headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+        headerImage={
+          <Image
+            source={require('@/assets/images/partial-react-logo.png')}
+            style={styles.reactLogo}
+          />
+        }>
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="title">Welcome!</ThemedText>
+          <HelloWave />
+        </ThemedView>
+        <AppKitButton />
+      </ParallaxScrollView>
+
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
