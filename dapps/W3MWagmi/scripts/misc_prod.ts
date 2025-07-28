2c80cf18c263f268189b95a966aeae81c71a1524
nacho <25931366+ignaciosantise@users.noreply.github.com>
2025-07-28 17:38:19 -0300

    chore: appkit bump

--- dapps/W3MWagmi/scripts/misc_prod.ts
+++ dapps/W3MWagmi/scripts/misc_prod.ts
@@ -11,15 +11,3 @@ export const getMetadata = () => {
     },
   };
 };
-
-export const getCustomWallets = () => {
-  const wallets = [
-    {
-      id: 'phantom-wallet',
-      name: 'Phantom',
-      image_url: 'https://avatars.githubusercontent.com/u/124594793?s=200&v=4',
-      mobile_link: 'phantom://',
-  }];
-
-  return wallets;
-};
