1637a2e59c8ceff68c71f95fb5ef101bdeda3ea8
nacho <25931366+ignaciosantise@users.noreply.github.com>
2025-09-22 14:32:25 -0300

    chore: enabled logs and socket status checker

--- dapps/W3MWagmi/src/hooks/useSocketStatus.ts
+++ dapps/W3MWagmi/src/hooks/useSocketStatus.ts
@@ -1,44 +1,32 @@
 import SettingsStore from '@/stores/SettingsStore';
-import {useEffect, useState} from 'react';
+import {useEffect} from 'react';
 import {useSnapshot} from 'valtio';
-import {useAccount} from 'wagmi';
 import {RELAYER_EVENTS} from '@walletconnect/core';
 import { ToastUtils } from '@/utils/ToastUtils';
+import { useProvider } from '@reown/appkit-react-native';
 
 export function useSocketStatus() {
-  const {connector} = useAccount();
-  const [provider, setProvider] = useState<any>(null);
+  const {provider} = useProvider();
   const {socketStatus} = useSnapshot(SettingsStore.state);
 
   useEffect(() => {
-    const getProvider = async () => {
-      if (connector && connector?.getProvider) {
-        const _provider = await connector?.getProvider();
-        setProvider(_provider);
-      }
-    };
-
-    getProvider();
-  }, [connector]);
-
-  useEffect(() => {
-    if (provider?.signer?.client?.core) {
-      provider?.signer?.client?.core.relayer.on(RELAYER_EVENTS.connect, () => {
+    if (provider?.client?.core) {
+      provider?.client?.core.relayer.on(RELAYER_EVENTS.connect, () => {
         ToastUtils.showSuccessToast('Network connection is restored!');
         SettingsStore.setSocketStatus('connected');
       });
-      provider.signer.client.core.relayer.on(RELAYER_EVENTS.disconnect, () => {
+      provider?.client?.core.relayer.on(RELAYER_EVENTS.disconnect, () => {
         ToastUtils.showErrorToast('Network connection lost.', 'Please check your internet connection.');
         SettingsStore.setSocketStatus('disconnected');
       });
-      provider.signer.client.core.relayer.on(
+      provider?.client?.core.relayer.on(
         RELAYER_EVENTS.connection_stalled,
         () => {
           ToastUtils.showErrorToast('Network connection stalled.', 'Please check your internet connection.');
           SettingsStore.setSocketStatus('stalled');
         },
       );
-      provider.signer.client.core.relayer.on(
+      provider?.client?.core.relayer.on(
         RELAYER_EVENTS.transport_closed,
         () => {
           ToastUtils.showInfoToast('Network connection closed.');
