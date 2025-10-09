2670d4ee421c89c42c9a95282012c6b1107b42ee
nacho <25931366+ignaciosantise@users.noreply.github.com>
2025-10-09 17:44:04 -0300

    chore: updated wallet to RN 0.82 + deps to latest

--- wallets/rn_cli_wallet/src/utils/LinkingUtils.ts
+++ wallets/rn_cli_wallet/src/utils/LinkingUtils.ts
@@ -1,6 +1,5 @@
-import {Linking, Platform} from 'react-native';
+import {Linking} from 'react-native';
 import Toast from 'react-native-toast-message';
-import Minimizer from '@kangfenmao/react-native-minimizer';
 
 interface redirect {
   native?: string;
@@ -14,20 +13,20 @@ interface Props {
 }
 
 const goBackOrToast = () => {
-  if (Platform.OS === 'android') {
-    Toast.show({
-      type: 'success',
-      text1: 'Success!',
-      text2: 'Redirecting to the dapp',
-    });
-    Minimizer.goBack();
-  } else {
+  // if (Platform.OS === 'android') {
+  //   Toast.show({
+  //     type: 'success',
+  //     text1: 'Success!',
+  //     text2: 'Redirecting to the dapp',
+  //   });
+  //   // Minimizer.goBack();
+  // } else {
     Toast.show({
       type: 'success',
       text1: 'Success!',
       text2: 'Please go back to the dapp',
     });
-  }
+  // }
 };
 
 export const handleRedirect = ({peerRedirect, isLinkMode}: Props) => {
