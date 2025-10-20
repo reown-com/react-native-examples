fe66e8acbe319c407dcdb25a04aff274972d1e37
kewe63 <86300262+Kewe63@users.noreply.github.com>
2025-10-20 19:46:42 +0300

    Update metro.config.js

--- dapps/W3MEthers/metro.config.js
+++ dapps/W3MEthers/metro.config.js
@@ -3,7 +3,7 @@ const { mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://metrobundler.dev/docs/configuration/
  *
  * @type {import('metro-config').MetroConfig}
  */
