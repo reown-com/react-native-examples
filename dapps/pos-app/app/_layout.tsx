899897c13a98c3a54fb960f929d054d326e82cc6
nacho <25931366+ignaciosantise@users.noreply.github.com>
2025-10-27 15:37:05 -0300

    chore: minor changes + added pos action workflow

--- dapps/pos-app/app/_layout.tsx
+++ dapps/pos-app/app/_layout.tsx
@@ -17,6 +17,26 @@ import { appKit, wagmiAdapter } from "@/utils/appkit";
 import { AppKit, AppKitProvider } from "@reown/appkit-react-native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
+import * as Sentry from "@sentry/react-native";
+
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 const queryClient = new QueryClient();
 
@@ -24,7 +44,7 @@ export const unstable_settings = {
   anchor: "(tabs)",
 };
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
   const { posClient, isInitialized } = useInitializePOS({
     projectId: process.env.EXPO_PUBLIC_PROJECT_ID!,
@@ -85,4 +105,4 @@ export default function RootLayout() {
       </WagmiProvider>
     </AppKitProvider>
   );
-}
+});
